56f974b8d44b8e898830814560e50b3dc24cd59d
AgustinGenovese <agusgenot@gmail.com>
2025-10-25 20:14:26 -0300

    fix: copiar _redirects en build para Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // 👇 esto asegura que el archivo _redirects se copie a /dist
+    viteStaticCopy({
+      targets: [
+        { src: 'public/_redirects', dest: '.' }
+      ],
+    }),
+  ],
   server: {
     port: 5173, // opcional: define el puerto de React
     proxy: {
