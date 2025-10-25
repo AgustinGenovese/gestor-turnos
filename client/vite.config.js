6033287835fd934bdd59be7e5dba4755fbd3e0d6
AgustinGenovese <agusgenot@gmail.com>
2025-10-25 20:27:44 -0300

    fix: sesión en producción

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // 👇 esto asegura que el archivo _redirects se copie a /dist
-    viteStaticCopy({
-      targets: [
-        { src: 'public/_redirects', dest: '.' }
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173, // opcional: define el puerto de React
     proxy: {
