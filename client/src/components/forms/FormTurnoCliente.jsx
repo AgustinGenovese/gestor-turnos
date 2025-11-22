e0180198f943d56768a916664f255c16ab182c18
AgustinGenovese <agusgenot@gmail.com>
2025-11-22 19:51:06 -0300

    style: agregar barra navegacion

--- client/src/components/forms/FormTurnoCliente.jsx
+++ client/src/components/forms/FormTurnoCliente.jsx
@@ -210,29 +210,23 @@ export function FormTurnoCliente({ onCrearTurno }) {
         className="flex flex-col w-full gap-6 px-0 py-4 rounded-2xl shadow-md"
         style={{ backgroundColor: "#0d0d12ff", color: "black" }}
       >
-        {/* 🔵 Barra de progreso */}
-        <div className="grid grid-cols-4 gap-2 mb-2">
-          {["Datos", "Servicio", "Fecha", "Horario"].map((nombre, index) => {
+
+        {/* Barra de progreso minimalista solo texto */}
+        <div className="grid grid-cols-4 gap-4 mb-2 text-center border-b border-gray-700 pb-2">
+          {["Datos", "Tipo", "Fecha", "Horario"].map((nombre, index) => {
             const numeroPaso = index + 1;
-            const activo = step >= numeroPaso;
+            const activo = step === numeroPaso;
 
             return (
-              <div
+              <span
                 key={index}
                 className={`
-          flex flex-col items-center p-2 rounded-lg text-xs font-medium
-          transition-all duration-300
-          ${activo ? "bg-[#c2a255] text-black" : "bg-gray-700 text-gray-300"}
+          py-2 text-sm font-semibold tracking-wide transition-all
+          ${activo ? "text-[#c2a255]" : "text-gray-400"}
         `}
               >
-                <div
-                  className={`w-6 h-6 flex items-center justify-center rounded-full mb-1 
-          ${activo ? "bg-black text-white" : "bg-gray-500 text-white"}`}
-                >
-                  {numeroPaso}
-                </div>
-                <span>{nombre}</span>
-              </div>
+                {nombre}
+              </span>
             );
           })}
         </div>
