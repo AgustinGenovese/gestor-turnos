6d463f18ff243b8c10644942479c6cfec3695ab6
AgustinGenovese <agusgenot@gmail.com>
2025-11-23 02:41:58 -0300

    feat: agregar link instagram y maps

--- client/src/pages/TurnosPage.jsx
+++ client/src/pages/TurnosPage.jsx
@@ -56,38 +56,53 @@ export default function TurnosPage() {
 
         <FormTurnoCliente onCrearTurno={crearTurno} />
 
-
         {/* DECORACIÓN INFERIOR */}
         <div className="mt-12 w-full flex justify-center space-x-4 text-gray-600">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-8 h-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+          <a
+            href="https://www.google.com/maps"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M14.752 11.168l-3.197-2.132a2 2 0 10-2.11 3.328l3.197 2.132a2 2 0 002.11-3.328zM12 21l-6-6 6-6 6 6-6 6z"
-            />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-8 h-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 2C8.686 2 6 4.686 6 8c0 4.5 6 12 6 12s6-7.5 6-12c0-3.314-2.686-6-6-6z"
+              />
+              <circle cx="12" cy="8.5" r="2.5" />
+            </svg>
+          </a>
+
+          <a
+            href="https://www.instagram.com/sarkirianbarberia/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M8 7V3m8 4V3M3 11h18M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <rect x="3" y="3" width="18" height="18" rx="5" ry="5"></rect>
+              <circle cx="12" cy="12" r="4"></circle>
+              <circle cx="17" cy="7" r="1.2"></circle>
+            </svg>
+          </a>
+
         </div>
+
       </div>
     </>
   );
